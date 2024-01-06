68b1ef18c7bd1a3d000f889e9801c4ad4080f37f
Ramasubramaniyam Azhagusundaram <rmsubramaniyam@gmail.com>
2024-01-06 09:13:27 -0800

    second

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ramsng/sub03
+
+go 1.21.5
+
+require github.com/ramsng/sub02 v0.0.0-20240104063154-d721adcc95ee
---
+++ sub04.go
@@ -0,0 +1,11 @@
+package sub04
+
+import (
+	"strings"
+
+	"github.com/ramsng/sub02"
+)
+
+func Praise() string {
+	return "\n Nice spin - Keep it up" + strings.ToUpper(sub02.Desc())
+}
