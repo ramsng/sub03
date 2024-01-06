bc3d0ffb083e932ef42a966ac9d4efbac1a88e84
Ramasubramaniyam Azhagusundaram <rmsubramaniyam@gmail.com>
2024-01-06 11:41:32 -0800

    12

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ramsng/sub03
 
 go 1.21.5
 
-require github.com/ramsng/sub02 v0.0.0-20240104063154-d721adcc95ee
+require github.com/ramsng/sub02 v0.0.0-20240106193659-eb953661b1c2
--- sub03.go
+++ sub03.go
@@ -1,11 +1,13 @@
 package sub03
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/ramsng/sub02"
 )
 
 func Praise() string {
+	fmt.Printf("comments in sub03", sub02.Desc())
 	return "\n Nice spin - Keep it up" + strings.ToUpper(sub02.Desc())
 }
